111d91821eea26c561ee9f1cde155f9f4c369b59
Richard Neumann <mail@richard-neumann.de>
2024-10-09 17:22:45 +0200

    Tear down project

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,1 @@
-/// I consent to the transfer of this crate to the first person who asks help@crates.io for it.
+//! I consent to the transfer of this crate to the first person who asks help@crates.io for it.
